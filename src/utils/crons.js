b97872c0c3be840d88d277d287cfad4e1c2461c0
Mayank Soni <mayank93soni@gmail.com>
2024-10-10 05:17:17 +0530

    call updateTime funtion in cronJob

--- src/utils/crons.js
+++ src/utils/crons.js
@@ -1,20 +1,18 @@
 import cron from "node-cron";
-import fetchCryptoData from "../controller/fetchDetails.js";
+import {
+  fetchCryptoData,
+  updatedDeviationDataTime,
+} from "../controller/fetchDetails.js";
 import { checkHealth } from "../db/index.js";
-// cron.schedule("* * * * *", () => {
-//   console.log("Fetching cryptocurrency data...");
-//   fetchCryptoData();
-// });
+
 cron.schedule("0 */2 * * *", () => {
   console.log("Fetching cryptocurrency data...");
-  if(checkHealth()){
-  fetchCryptoData();
-  }
-  else {
+  if (checkHealth()) {
+    fetchCryptoData(); // fetch data
+    updatedDeviationDataTime(); // update the data time
+  } else {
     console.log("Database is not working. Skipping data fetching...");
     //process.exit(1); or can shutdown the server
     return;
   }
 });
-
-
